5ea0cee57c4a6445c8cb636edcf8bdaaff0405b7
EkaterinaCheliadinova <ekote.98@gmail.com>
2024-12-08 17:44:58 +0100

    shema added

--- generate_json_files.py
+++ generate_json_files.py
@@ -56,3 +56,50 @@ for category, path in base_dirs.items():
         json.dump(json_data, f, ensure_ascii=False, indent=4)
 
 print("JSON files have been created.")
+
+
+
+{"@context":"https:\/\/schema.org","@graph":
+ [{"@type":"BreadcrumbList","@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#breadcrumblist",
+   "itemListElement":[{"@type":"ListItem","@id":"https:\/\/sassykoonz.com\/#listItem",
+                       "position":1,"name":"Home","item":"https:\/\/sassykoonz.com\/",
+                       "nextItem":{"@type":"ListItem",
+                                   "@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#listItem",
+                                   "name":"Maine Coon Kittens | Maine Coon Kittens for Sale"}},
+                                   {"@type":"ListItem",
+                                    "@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#listItem",
+                                    "position":2,"name":"Maine Coon Kittens | Maine Coon Kittens for Sale",
+                                    "previousItem":{
+                                        "@type":"ListItem",
+                                        "@id":"https:\/\/sassykoonz.com\/#listItem",
+                                        "name":"Home"}}]},
+    {"@type":"Organization",
+     "@id":"https:\/\/sassykoonz.com\/#organization",
+     "name":"Sassy Koonz Maine Coons",
+     "description":"Maine Coon Cats and Kittens",
+     "url":"https:\/\/sassykoonz.com\/",
+     "telephone":"+19043358845",
+     "logo":{"@type":
+             "ImageObject",
+             "url":"https:\/\/sassykoonz.com\/wp-content\/uploads\/2019\/08\/fullsizeoutput_2056.jpeg",
+             "@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#organizationLogo"},
+             "image":{"@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#organizationLogo"},
+             "sameAs":["https:\/\/facebook.com\/sassykoonz","https:\/\/instagram.com\/sassykoonz",
+                       "https:\/\/tiktok.com\/@sassykoonz","https:\/\/pinterest.com\/sassykoonz",
+                       "https:\/\/www.youtube.com\/channel\/UCeSjppoJ0-NWYMY9dnz9LTQ"]},
+                       {"@type":"WebPage","@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#webpage",
+                        "url":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/",
+                        "name":"Maine Coon Kittens For Sale: Meet Your New Family Member",
+                        "description":"Maine Coon Kittens For Sale. Raised in a home environment. Kittens are socialized, friendly and healthy. Stellar Health Guarantee",
+                        "inLanguage":"en-US","isPartOf":{"@id":"https:\/\/sassykoonz.com\/#website"},
+                        "breadcrumb":{"@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#breadcrumblist"},
+                        "image":{"@type":"ImageObject","url":"https:\/\/i0.wp.com\/sassykoonz.com\/wp-content\/uploads\/2024\/11\/Maine-Coon-Cattery-3.png?fit=2048%2C1152&ssl=1",
+                                 "@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#mainImage","width":2048,"height":1152,"caption":"maine coon cattery"},
+                                 "primaryImageOfPage":{"@id":"https:\/\/sassykoonz.com\/maine-coon-kittens-for-sale\/#mainImage"},
+                                 "datePublished":"2018-03-22T17:36:04-04:00","dateModified":"2024-11-25T14:29:28-05:00"},
+                                 {"@type":"WebSite","@id":"https:\/\/sassykoonz.com\/#website","url":"https:\/\/sassykoonz.com\/",
+                                  "name":"Sassy Koonz Maine Coon Cattery | Maine Coon Kittens",
+                                  "description":"Maine Coon Cats and Kittens",
+                                  "inLanguage":"en-US",
+                                  "publisher":{"@id":"https:\/\/sassykoonz.com\/#organization"}}]}
+		
